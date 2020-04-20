6675b41999ada8b7e8d5064b366e509cc7bd3611
Piotr Obst <obsttube@gmail.com>
2020-04-21 01:25:22 +0200

    wyk 3, zad 2 #23

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ app.patients=[]
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    print("OISDNFINBSDIFNISDNIFNSIUDNFSNDUIFNIUDSN")
     return JSONResponse(
         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
         content=jsonable_encoder({"detail": exc.errors(), "body": exc.body}),
