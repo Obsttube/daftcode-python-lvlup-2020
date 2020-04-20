a69880cfc67e48f70e2bdfa1e9f16c4ee4d00cb5
Piotr Obst <obsttube@gmail.com>
2020-04-21 00:09:01 +0200

    wyk 3, zad 2 #9

--- main.py
+++ main.py
@@ -20,10 +20,10 @@ def welcome():
 
 @app.post("/login")
 def create_cookie(response: Response, login: str = None, password: str = Query(None, alias="pass")):
-    response.status_code = status.HTTP_302_FOUND#HTTP_401_UNAUTHORIZED
+    #response.status_code = status.HTTP_302_FOUND#HTTP_401_UNAUTHORIZED
     #session_token = sha256(bytes(f"{login}{password}{app.secret_key}", encoding='utf8')).hexdigest()
     #response.set_cookie(key="session_token", value=session_token)
-    #return RedirectResponse(url="/welcome", status_code=status.HTTP_302_FOUND)
+    return RedirectResponse(url="/hello", status_code=status.HTTP_302_FOUND)
 
 @app.get("/method")
 @app.post("/method")
