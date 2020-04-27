2cf63a615220d820ec1b0dd778e94496d19f6ad4
Piotr Obst <obsttube@gmail.com>
2020-04-28 00:30:13 +0200

    wyk 4, zad 3 #10

--- routers/lecture4.py
+++ routers/lecture4.py
@@ -55,8 +55,8 @@ async def tracks_composers(response: Response, album_id: int):
 	router.db_connection.row_factory = aiosqlite.Row
 	cursor = await router.db_connection.execute("SELECT * FROM albums WHERE AlbumId = ?",
 		(album_id, ))
-	album = await cursor.fetchone()
-	print(dict(album)) #{'AlbumId': 348, 'Title': 'Mandaryna HITS', 'ArtistId': 1}
+	album = await cursor.fetchall()
+	#print(dict(album)) #{'AlbumId': 348, 'Title': 'Mandaryna HITS', 'ArtistId': 1}
 	'''if album is None:
 		response.status_code = status.HTTP_404_NOT_FOUND
 		return {"detail":{"error":"Album with that ID does not exist."}}'''
